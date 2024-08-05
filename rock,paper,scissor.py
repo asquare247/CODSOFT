da586d11d3efd8e1177c03417ec603ac2eec5517
Ayush Arya <150467837+asquare247@users.noreply.github.com>
2024-08-05 13:17:02 +0530

    rock,paper,scissor.py

--- rock,paper,scissor.py
+++ rock,paper,scissor.py
@@ -1,19 +1,29 @@
 import random
-a=input("choose one:\n 1.rock\n 2.paper\n 3.scissor\n Your choice: ")
-s=['rock','paper','scissor']
-ai=random.choice(s)
-print("Computer choice: ",ai)
+while True:
+    a=input("choose one:\n 1.rock\n 2.paper\n 3.scissor\n")
+    print( "Your choice: ",a )
+    s=['rock','paper','scissor']
+    ai=random.choice(s)
+    print("Computer choice: ",ai)
 
-if a==ai:
-    print("Tie")
-elif a=='rock' and ai=='scissors':
-    print("You win")
-elif a=='paper' and ai=='rock':
-    print("You win")
-elif a=='scissor' and ai=='paper':
-    print("You win")
-else:
-    print("You lose")
+    if a==ai:
+        print("Tie")
+    elif a=='rock' and ai=='scissors':
+        print("You win")
+    elif a=='paper' and ai=='rock':
+        print("You win")
+    elif a=='scissor' and ai=='paper':
+        print("You win")
+    else:
+        print("You lose")
+
+    play=input("play again:(y/n)\n")
+    if play=="y":
+        continue
+    elif play=="n":
+        break
+print("Thanks for playing")
+
 
 
 
